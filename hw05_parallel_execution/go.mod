878ec494745bc82f0ddaa9cf76ab890542d2454e
Ayna5 <aina2100@yandex.ru>
2021-02-22 20:30:45 +0300

    [hw05_parallel_execution]

--- hw05_parallel_execution/go.mod
+++ hw05_parallel_execution/go.mod
@@ -1,10 +1,10 @@
-module github.com/fixme_my_friend/hw05_parallel_execution
+module github.com/Ayna5/hw-golangOtus/hw05_parallel_execution
 
 go 1.15
 
 require (
-	github.com/stretchr/testify v1.5.1
-	go.uber.org/goleak v1.0.0
+	github.com/golangci/golangci-lint v1.37.1 // indirect
+	github.com/stretchr/testify v1.7.0
+	go.uber.org/goleak v1.1.10
 	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
-	golang.org/x/tools v0.0.0-20200426102838-f3a5411a4c3b // indirect
 )
--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -1,15 +1,92 @@
-package hw05_parallel_execution //nolint:golint,stylecheck
+package hw05_parallel_execution //nolint:golint,stylecheck,revive
 
 import (
+	"context"
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+
+type Workers struct {
+	countGoroutines      int
+	maxErrCount   int
+	tasks         []Task
+	mu            *sync.Mutex
+	completedTask int
+	errCount      int
+}
 
 type Task func() error
 
 // Run starts tasks in N goroutines and stops its work when receiving M errors from tasks
 func Run(tasks []Task, N int, M int) error {
-	// Place your code here
+	workers := Workers{
+		countGoroutines:    N,
+		maxErrCount: M,
+		tasks:       tasks,
+		mu:          &sync.Mutex{},
+	}
+	err := workers.RunTasks()
+	return err
+}
+
+func (w *Workers) RunTasks() error {
+	var wg sync.WaitGroup
+	taskChan := make(chan Task, len(w.tasks))
+	errChan := make(chan bool)
+	_, cancel := context.WithCancel(context.Background())
+
+	for _, task := range w.tasks {
+		taskChan <- task
+	}
+
+	for i := 0; i < w.countGoroutines; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for task := range taskChan {
+				if w.errLimitCheck() {
+					err := task()
+					errChan <- err == nil
+				}
+			}
+		}()
+	}
+
+	go func() {
+		for ok := range errChan {
+			w.completedTask++
+			if !ok {
+				w.mu.Lock()
+				w.errCount++
+				w.mu.Unlock()
+				if !w.errLimitCheck() {
+					cancel()
+				}
+			}
+			if w.completedTask == len(w.tasks) {
+				cancel()
+			}
+		}
+	}()
+
+	close(taskChan)
+	wg.Wait()
+	close(errChan)
+
+	if !w.errLimitCheck() {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
+}
+
+func (w *Workers) errLimitCheck() bool {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	if w.maxErrCount <= 0 || w.errCount >= w.maxErrCount {
+		return false
+	}
+	return true
 }

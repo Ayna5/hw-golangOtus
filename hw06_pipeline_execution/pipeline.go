ab4b4888b071e9c13cd8579c690ff0f496fbf4e2
Ayna5 <aina2100@yandex.ru>
2021-02-23 19:05:54 +0300

    [hw06_pipeline_execution] hw06

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,11 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
+	if in == nil {
+		ch := make(Bi)
+		defer close(ch)
+		return ch
+	}
 	for _, stage := range stages {
 		ch := make(Bi)
 		go func(ch Bi, in In) {

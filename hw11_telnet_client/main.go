9eb16a31f290a1c1d5b102133b6f4df0fd350361
Ayna5 <aina2100@yandex.ru>
2021-03-28 12:11:09 +0300

    [hw11_telnet_client] added draft

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,49 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+var (
+	timeout    time.Duration
+	host, port string
+)
+
+func init() {
+	flag.DurationVar(&timeout, "timeout", 10*time.Second, "timeout")
+	flag.StringVar(&host, "host", "localhost", "host")
+	flag.StringVar(&port, "port", "9000", "port")
+}
+
 func main() {
-	// Place your code here
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	flag.Parse()
+
+	addr := net.JoinHostPort(host, port)
+	client := NewTelnetClient(addr, timeout, os.Stdin, os.Stdout)
+
+	err := client.Connect()
+	if err != nil {
+		fmt.Errorf("cannot connect: %w", err)
+	}
+	defer client.Close()
+
+	go func() {
+		err := client.Receive()
+		if err != nil {
+			fmt.Errorf("can't receieve: %v", err)
+			return
+		}
+	}()
+
+	go func() {
+		err := client.Send()
+		if err != nil {
+			fmt.Errorf("can't send: %v", err)
+			return
+		}
+	}()
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,18 +1,64 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
 type TelnetClient interface {
-	// Place your code here
+	Connect() error
+	Send() error
+	Receive() error
+	Close() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here
+type telnetClient struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
+
+func (t telnetClient) Connect() error {
+	conn, err := net.DialTimeout("tcp", t.address, t.timeout)
+	if err != nil {
+		return fmt.Errorf("cannot connect: %v", err)
+	}
+	t.conn = conn
 	return nil
 }
 
-// Place your code here
-// P.S. Author's solution takes no more than 50 lines
+func (t telnetClient) Send() error {
+	_, err := io.Copy(t.conn, t.in)
+	if err != nil {
+		return fmt.Errorf("cannot send: %w", err)
+	}
+	return nil
+}
+
+func (t telnetClient) Receive() error {
+	_, err := io.Copy(t.out, t.conn)
+	if err != nil {
+		return fmt.Errorf("cannot receive: %w", err)
+	}
+	return nil
+}
+
+func (t telnetClient) Close() error {
+	if t.conn != nil {
+		return t.conn.Close()
+	}
+	return nil
+}
+
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+	return &telnetClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
